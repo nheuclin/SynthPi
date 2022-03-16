6fefa53f6ad0f70410cc329220b00f0e11ed59be
AaronCollins1999 <aaroncollins1999@gmail.com>
2022-03-16 13:51:34 +0000

    Added display library files
    library designed to translate CC values and send them via SPI to the MAX7219 chip that is used to power the 7segment display or this device.

---
+++ src/display.cpp
@@ -0,0 +1,220 @@
+#define MAX7219_REG_NOOP 0x0
+#define MAX7219_REG_DIGIT0 0x1
+#define MAX7219_REG_DIGIT1 0x2
+#define MAX7219_REG_DIGIT2 0x3
+#define MAX7219_REG_DIGIT3 0x4
+#define MAX7219_REG_DIGIT4 0x5
+#define MAX7219_REG_DIGIT5 0x6
+#define MAX7219_REG_DIGIT6 0x7
+#define MAX7219_REG_DIGIT7 0x8
+#define MAX7219_REG_DECODEMODE 0x09
+#define MAX7219_REG_INTENSITY 0xA
+#define MAX7219_REG_SCANLIMIT 0xB
+#define MAX7219_REG_SHUTDOWN 0xC
+#define MAX7219_REG_DISPLAYTEST 0xF
+
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
+
+#include "spitest.h"
+
+using namespace SYNTHPI;
+
+// MAX7219
+
+MAX7219::MAX7219(){
+
+	
+	 //fd = open(settings.spiDevice.c_str(), O_RDWR);
+        fd = open("/dev/spidev0.0", O_RDWR);
+		std::cout << "devices opened: " << std::endl;
+	if (fd < 0){
+		throw "Can't open device";
+	}
+	// set SPI mode
+	int ret = ioctl(fd, SPI_IOC_WR_MODE, 1);
+	std::cout << "Spi mode set " << std::endl;
+	if (ret == -1) {
+		//throw "can't set spi mode";
+	}
+	std::cout << "device setup" << std::endl;
+
+
+     //Allocate digit buffer and init to 0
+     digitBuffer.assign(numDigits, 0);
+
+
+
+    //Set command regs
+    command(MAX7219_REG_DECODEMODE, decodeMode);
+    command(MAX7219_REG_INTENSITY, intensity);
+    command(MAX7219_REG_SCANLIMIT, scanLimit);
+    command(MAX7219_REG_SHUTDOWN, shutdown);
+    command(MAX7219_REG_DISPLAYTEST, displayTest);
+
+	std::cout << "device setup" << std::endl;
+}
+
+
+void MAX7219::command(unsigned char reg, unsigned char data) {
+	std::cout << "command " <<reg <<"  "<< data << std::endl;
+    unsigned char tx[2] = {reg, data};
+    spi_transfer(fd, tx, 2);
+}
+
+int MAX7219::spi_transfer(int fd, unsigned char* tx, int n) {
+	std::cout << "transfered: "<< tx << std::endl;
+	struct spi_ioc_transfer tr;
+	memset(&tr,0,sizeof(struct spi_ioc_transfer));
+	tr.tx_buf = (unsigned long)tx;
+	//tr.rx_buf = (unsigned long)rx;
+	tr.len = n;
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = bpw;	
+	int ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	return ret;
+}
+
+
+// HIGH LEVEL METHODS //
+
+void MAX7219::flush() {
+    //unsigned char tx[2]; // Holds digit address and digit value
+    for (unsigned char digit = 0; digit < 8; digit ++) {
+        command((digit+1), digitBuffer[digit]);
+    }
+}
+
+void MAX7219::clear(bool redraw) {
+    for (unsigned int digit = 0; digit < (numDigits/2); digit ++)
+        digitBuffer[digit] = 0x0;
+    if(redraw) flush();
+}
+
+// SETTERS //
+
+void MAX7219::setDigit(unsigned int digit, unsigned char value, bool redraw) {
+    digitBuffer[digit] = value;
+    if(redraw) flush();
+}
+
+void MAX7219::setDecodeMode(unsigned char value){
+    decodeMode = value;
+    command(MAX7219_REG_DECODEMODE, value);
+}
+void MAX7219::setIntensity(unsigned char value){
+    intensity = value;
+    command(MAX7219_REG_INTENSITY, value);
+}
+void MAX7219::setScanLimit(unsigned char value){
+    scanLimit = value;
+    command(MAX7219_REG_SCANLIMIT, value);
+}
+void MAX7219::setShutdown(unsigned char value){
+    shutdown = value;
+    command(MAX7219_REG_SHUTDOWN, value);
+}
+void MAX7219::setDisplayTest(unsigned char value){
+    displayTest = value;
+    command(MAX7219_REG_DISPLAYTEST, value);
+}
+
+// GETTERS //
+
+unsigned char MAX7219::getDigit(unsigned char digit) {
+    return digitBuffer[digit];
+}
+
+unsigned char MAX7219::getDecodeMode(){
+    return decodeMode;
+}
+unsigned char MAX7219::getIntensity(){
+    return intensity;
+}
+unsigned char MAX7219::getScanLimit(){
+    return scanLimit;
+}
+unsigned char MAX7219::getShutdown(){
+    return shutdown;
+}
+unsigned char MAX7219::getDisplayTest(){
+    return displayTest;
+}
+unsigned int MAX7219::getNumDigits() {
+    return numDigits;
+}
+
+//End MAX7219 
+
+// Display
+
+// Constructor & Destructor //
+
+Display::Display(){
+
+}
+
+// PRIVATE METHODS
+
+void Display::setThreeDigit(unsigned int value) {
+    unsigned char digitOne, digitTwo, digitThree;
+    digitOne = decHexVals[value % 10];
+    digitTwo = decHexVals[(value/10) % 10];
+    digitThree = decHexVals[(value/100) % 10];
+
+    setDigit(0, digitOne, false);
+    setDigit(1, digitTwo, false);
+    setDigit(2, digitThree, false);
+    flush();
+}
+
+void Display::setTwoDigit(unsigned int value) {
+    unsigned char digitOne, digitTwo;
+    digitOne = decHexVals[value % 10];
+    digitTwo = decHexVals[(value/10) % 10];
+
+    setDigit(0, digitOne, false);
+    setDigit(1, digitTwo, false);
+}
+
+void Display::setOneDigit(unsigned int value) {
+    unsigned char digitOne;
+    digitOne = decHexVals[value % 10];
+
+    setDigit(0, digitOne, false);
+}
+
+void Display::setVal(unsigned int value, bool redraw) {
+    clear(false);
+    // Separate value into individual digits
+    // 3 digits:
+    if(value > 99) setThreeDigit(value);
+    // 2 digits:
+    else if ((value > 9) && (value <= 99)) setTwoDigit(value);
+    // 1 digit:
+    else if (value <= 9) setOneDigit(value);
+    if (redraw) flush();
+}
+
+void Display::setParam(unsigned int param, bool redraw) {
+    clear(false);
+    std::vector<unsigned char> parameter = params[param];
+    setDigit(7, parameter[0], false);
+    setDigit(6, parameter[1], false);
+    setDigit(5, parameter[2], false);
+    setDigit(4, parameter[3], false);
+    if (redraw) flush();
+}
+
+void Display::ccToDisplay(unsigned int address, unsigned int value){
+    if (currentParam != address){
+        setParam(address, true);
+        currentParam = address;
+    }
+    setVal(value, true);
+
+}
+
+//Display::~Display(){
+
+//}
---
+++ src/display.h
@@ -0,0 +1,240 @@
+#ifndef SYNTHPI_DISPLAY
+#define SYNTHPI_DISPLAY
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <thread>  
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+
+namespace SYNTHPI{
+
+
+// struct MAX7219settings {
+// 	/**
+// 	 * The SPI device in /dev used.
+// 	 **/
+// 	std::string spiDevice = "/dev/spidev0.0";
+// };
+
+class MAX7219 {
+
+    public:
+        MAX7219();
+
+        // ~MAX7219();
+
+        std::vector<unsigned char> digitBuffer;
+
+        int spi_transfer(int fd, unsigned char* tx, int n);
+
+        void command(unsigned char reg, unsigned char data);
+
+        uint32_t speed = 500000;
+        uint16_t delay = 0;
+        int fd;
+        uint8_t bpw   = 8;
+
+        unsigned char decodeMode = 0x0;
+        unsigned char intensity = 0x7;
+        unsigned char scanLimit = 0x7;
+        unsigned char shutdown = 0x1;
+        unsigned char displayTest = 0x0;
+        unsigned int numDigits = 8;
+
+        //SETTERS //
+
+       /**
+         * Sets a value in the \ref digitBuffer.
+         *
+         * @param digit Byte address of digit.
+         * @param value Byte value to assign.
+         * @param redraw If true, updates all digits at the end of method call.
+         */
+        void setDigit(unsigned int digit, unsigned char value, bool redraw);
+
+          /**
+         * Sets the \ref decodeMode.
+         * @param value Decode mode byte.
+         */
+        void setDecodeMode(unsigned char value);
+
+        /**
+         * Sets the \ref intensity.
+         * @param value Intensity value (1-15).
+         */
+        void setIntensity(unsigned char value);
+
+        /**
+         * Sets the \ref scanLimit.
+         * @param value Scan limit (0-7).
+         */
+        void setScanLimit(unsigned char value);
+
+        /**
+         * Sets the \ref shutdown mode.
+         * @param value Shutdown mode (1/0).
+         */
+        void setShutdown(unsigned char value);
+
+        /**
+         * Sets the \ref displayTest mode.
+         * @param value Display test mode (1/0).
+         */
+        void setDisplayTest(unsigned char value);
+
+         // GETTERS //
+
+        /**
+         * Gets a value of a digit in the \ref digitBuffer.
+         * @param digit Byte address of digit.
+         * @returns Current byte value of digit.
+         */
+        unsigned char getDigit(unsigned char digit);
+
+        /**
+         * Gets the current \ref decodeMode.
+         * @returns Current \ref decodeMode.
+         */
+        unsigned char getDecodeMode();
+
+        /**
+         * Gets the current \ref intensity.
+         * @returns Current \ref intensity.
+         */
+        unsigned char getIntensity();
+
+        /**
+         * Gets the current \ref scanLimit.
+         * @returns Current \ref scanLimit.
+         */
+        unsigned char getScanLimit();
+
+        /**
+         * Gets the current \ref shutdown mode.
+         * @returns Current \ref shutdown mode.
+         */
+        unsigned char getShutdown();
+
+        /**
+         * Gets the current \ref displayTest mode.
+         * @returns Current \ref displayTest mode.
+         */
+        unsigned char getDisplayTest();
+
+        /**
+         * Gets the number of digits in the display.
+         * @returns Number of display digits.
+         */
+        unsigned int getNumDigits();
+       
+        // HIGH LEVEL METHODS //
+
+        /**
+            * Writes all \ref digitBuffer values to display via SPI bus.
+            */
+        void flush();
+
+        /**
+            * Resets all digits in the \ref digitBuffer to 0.
+            * @param redraw If true, calls \ref flush to update display.
+            */
+        void clear(bool redraw);
+
+};
+
+
+class Display: public MAX7219 {
+
+    private:
+
+        /** Vector to contain corresponding hex values
+         * for decimal digit representation.
+         *
+         * Prevents the need to toggle Code B decode
+         * for every numerical display.
+         */
+        const std::vector<unsigned char> decHexVals = {
+            0x7E, // '0'
+            0x30, // '1'
+            0x6D, // '2'
+            0x79, // '3'
+            0x33, // '4'
+            0x5B, // '5'
+            0x5F, // '6'
+            0x70, // '7'
+            0x7F, // '8'
+            0x7B, // '9'
+        };
+
+        const std::vector<std::vector<unsigned char>> params = {
+            {0x3E,0x1D,0x0E,0x00}, // VOL
+            {0x77,0x0F,0x77,0x4E}, // BLND
+            {0x77,0x0F,0x77,0x4E}, // ATAC
+            {0x3D,0x4E,0x77,0x3B}, // DCAY
+            {0x5B,0x1C,0x5B,0x0F}, // SUST
+            {0x05,0x4F,0x0E,0x00}, // REL
+            {0x1F,0x77,0x15,0x37}, // BANK
+            
+        };
+
+        unsigned int currentParam = 0;
+
+    
+        /** Sets a 3 digit decimal value.
+         * @param value 3 digit value to set.
+         */
+        void setThreeDigit(unsigned int value);
+
+        /** Sets a 2 digit decimal value.
+         * @param value 2 digit value to set.
+         */
+        void setTwoDigit(unsigned int value);
+
+        /** Sets a 1 digit decimal value.
+         * @param value 1 digit value to set.
+         */
+        void setOneDigit(unsigned int value);
+
+
+
+    public:
+
+        Display(); ///Constructor.
+
+
+        /**
+         * Sets values in \ref digitBuffer
+         * to display a number up to 999.
+         *
+         * @param value Numerical value to display.
+         * @param redraw If true, updates display.
+         */
+        void setVal(unsigned int value, bool redraw);
+
+        /**
+         * Sets values in \ref digitBuffer
+         * to display which parameter is being changed by the MIDI keyboard.
+         *
+         * @param param Numerical representation of which MIDI parameter is being changed.
+         * @param redraw If true, updates display.
+         */
+        void setParam(unsigned int param, bool redraw);
+
+       // ~Display(); ///Destructor.
+
+
+};
+
+} //close namespace
+
+ #endif
