79aa12a444ef3f82f322aeb16a76cab5eb4846d6
nheuclin <74470670+nheuclin@users.noreply.github.com>
2022-03-23 12:58:38 +0000

    Update close_jackd.bash

--- scripts/close_jackd.bash
+++ scripts/close_jackd.bash
@@ -5,7 +5,7 @@ echo "Closing JACK server..."
 killall -s SIGINT jackd
 
 # Pause to let bash catch its breath
-sleep 1
+sleep 2
 
 # Inform the user the JACK server has closed
 echo "JACK server closed."
